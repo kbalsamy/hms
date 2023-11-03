4b674d9cdc4e7952e0a9a224e18856232304d694
subin1998 <developtothrive@gmail.com>
2023-11-03 14:22:42 +0000

    Project Initialised

---
+++ app/src/test/java/com/hygieia/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hygieia.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
