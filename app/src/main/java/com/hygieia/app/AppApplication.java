39669087ac8330850b19e576f2898a24e60940dc
kbalsamy <bkarthikonmail@gmail.com>
2023-11-12 21:53:47 +0000

    Logger implemented

--- app/src/main/java/com/hygieia/app/AppApplication.java
+++ app/src/main/java/com/hygieia/app/AppApplication.java
@@ -3,11 +3,26 @@ package com.hygieia.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hygieia.AppLogger.AppLogger;
+
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class AppApplication {
 
+	public AppLogger logger;
+	
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	}
+
+	@PostConstruct
+	public void initLogger() {
+		//initialize logger here
+		logger = AppLogger.getInstance();
+		logger.logInfo("Logger initialized");
+
 	}
 
 }
