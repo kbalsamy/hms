a830d464be3c5936ed2860a5c212468581de8cce
AswathyPappachan <aswathymp20@gmail.com>
2023-11-12 16:03:32 +0000

    Changed: User Repository

--- app/src/main/java/com/hygieia/Repositories/UserRepository.java
+++ app/src/main/java/com/hygieia/Repositories/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.hygieia.Entities.User;
 
-public interface UserRepository  extends JpaRepository<User,UUID>{
+public interface UserRepository  extends JpaRepository<User,Integer>{
 
     @Query(value = "select * from users u where u.email=:email", nativeQuery=true)
     User findByEmail(@Param("email") String email);
