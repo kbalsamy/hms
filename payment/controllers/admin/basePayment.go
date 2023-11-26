dabd946002e2bd5646b2445eccaad6a35f35efcc
ahua52 <1468492018@qq.com>
2023-11-26 21:53:36 +0000

    edit payMethod map and modify return structure

--- payment/controllers/admin/basePayment.go
+++ payment/controllers/admin/basePayment.go
@@ -1,6 +1,7 @@
 package admin
 
 import (
+	"math/rand"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,13 +17,13 @@ type BasePaymentControl struct {
 func (con BasePaymentControl) Pay(c *gin.Context) {
 	payMethod := c.Query("payMethod")
 	/*
-	* {1:debit, 2:credit, 3:mobile}
+	* {'DEBIT_CARD':debit, 'CREDIT_CARD':credit, 'MOBILE_PAY':mobile}
 	 */
-	if payMethod == "1" {
+	if payMethod == "DEBIT_CARD" {
 		DebitCardAccountController{}.Pay(c)
-	} else if payMethod == "2" {
+	} else if payMethod == "CREDIT_CARD" {
 		CreditCardAccountController{}.Pay(c)
-	} else if payMethod == "3" {
+	} else if payMethod == "MOBILE_PAY" {
 		MobileAccountController{}.Pay(c)
 	} else {
 		c.String(http.StatusOK, "wrong payment method")
@@ -30,15 +31,16 @@ func (con BasePaymentControl) Pay(c *gin.Context) {
 }
 
 func (con BasePaymentControl) success(c *gin.Context) {
-	c.String(http.StatusOK, "success")
-	// {
-	// 	paymentReferenceId: random
-	// 	message: "success"
-	// }
+	c.JSON(http.StatusOK, gin.H{
+		"message":            "success",
+		"paymentReferenceId": rand.Intn(10000000),
+	})
 }
 func (con BasePaymentControl) error(c *gin.Context, errInfo string) {
 	if errInfo == "" {
 		errInfo = "error"
 	}
-	c.String(http.StatusOK, errInfo)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "failure",
+	})
 }
